075850d09f2fe6b8e03fee165c3504004e9262f2
kipway <kipway@outlook.com>
2017-12-30 15:42:22 +0800

    Add c11_event.h & c11_thread.h

---
+++ ec/c11_event.h
@@ -0,0 +1,53 @@
+﻿/*!
+\file c11_event.h
+\brief 	event use C11
+
+ec library is free C++ library.
+
+\author	 kipway@outlook.com
+*/
+#pragma once
+#include <mutex>
+#include <condition_variable>
+namespace ec {
+	class cEvent
+	{
+	public:
+		cEvent(bool bInitiallyOwn = false, bool bManualReset = false)
+		{
+			_nready = bInitiallyOwn;
+			_bManualReset = bManualReset;
+		}
+		bool SetEvent() {
+			std::unique_lock<std::mutex> lck(_mtx);
+			_nready = true;
+			_cv.notify_one();
+			return true;
+		};
+		bool ResetEvent() {
+			std::unique_lock<std::mutex> lck(_mtx);
+			_nready = false;
+			return true;
+		}
+		bool Wait(int milliseconds)
+		{
+			std::unique_lock<std::mutex> lck(_mtx);
+			if (_cv.wait_for(lck, std::chrono::milliseconds(milliseconds)) != std::cv_status::timeout)
+			{
+				if (_nready)
+				{
+					if (!_bManualReset)
+						_nready = false;
+					return true;
+				}
+			}
+			return false;
+		}
+	protected:
+		bool _nready;
+		bool _bManualReset;
+		std::mutex _mtx;
+		std::condition_variable _cv;
+	};
+}
+
---
+++ ec/c11_thread.h
@@ -0,0 +1,98 @@
+﻿/*
+\file c11_thread.h
+\brief thread class use C11
+
+ec library is free C++ library.
+
+\author	 kipway@outlook.com
+*/
+#pragma once
+#include <thread>
+#include <atomic>
+#include <chrono>
+#include "c11_event.h"
+namespace ec
+{
+	class cThread
+	{
+	public:
+		cThread() :_bRuning(0), _bKilling(0)
+		{
+			_pevt = nullptr;
+			_pdojob = nullptr;
+			_pdoarg = nullptr;
+			_pthread = nullptr;
+		};
+		virtual ~cThread() {
+			if (_pthread != nullptr)
+				delete _pthread;
+		}
+		bool IsRun() { return 0 != _bRuning; };
+	protected:
+		std::atomic_int	_bRuning;
+		std::atomic_int	_bKilling;
+
+		std::thread *_pthread;
+		cEvent*		 _pevt;
+		void		*_pdoarg;
+		bool(*_pdojob)(void *); //return false will stop thread    
+	public:
+		void StartThread(cEvent* pevt, bool(*dojob)(void *) = nullptr, void*  pargs = nullptr)
+		{
+			if (_bRuning)
+				return;
+			_pevt = pevt;
+			_pdojob = dojob;
+			_pdoarg = pargs;
+			_pthread = new std::thread(ThreadProcess, this);
+		}
+		void StopThread()
+		{
+			if (nullptr != _pthread) {
+				_bKilling = 1;
+				while (_bRuning)
+					std::this_thread::sleep_for(std::chrono::milliseconds(100));
+				_pthread->join();
+				delete _pthread;
+				_pthread = nullptr;
+			}
+		}
+		bool Killing() { return 0 != _bKilling; };
+	private:
+		static void ThreadProcess(void* pargs)
+		{
+			cThread* pt = (cThread*)pargs;
+			pt->mainloop();
+		}
+	public:
+		void	mainloop()
+		{
+			OnStart();
+			_bKilling = 0;
+			_bRuning = 1;
+			while (!_bKilling)
+			{
+				if (!_pevt || _pevt->Wait(100)) {
+					if (!_pdojob)
+						dojob();
+					else
+					{
+						if (!_pdojob(_pdoarg))
+							break;
+					}
+				}
+				if (_pevt)
+					On100msTimer();
+			}
+			OnStop();
+			_bRuning = 0;
+			_bKilling = 0;
+		}
+	protected:
+		virtual bool OnStart() { return true; };
+		virtual void OnStop() { };
+		virtual	void dojob() { std::this_thread::sleep_for(std::chrono::milliseconds(100)); };
+		virtual void On100msTimer() { };
+	};
+}
+
