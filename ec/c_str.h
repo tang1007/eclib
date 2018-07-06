b60ef577cc7a709f944f639953b64d0285076c3d
kipway <kipway@outlook.com>
2018-07-06 11:21:01 +0800

    fix str_ieq(),str_neq()

--- ec/c_str.h
+++ ec/c_str.h
@@ -129,9 +129,10 @@ namespace ec
 	inline bool str_neq(const char* s1, const char* s2, size_t n)
 	{
 		size_t i = 0;
-		while (i++ < n && *s1 && *s2) {
+		while (i < n && *s1 && *s2) {
 			if (*s1++ != *s2++)
 				return false;
+			i++;
 		}
 		return i == n;
 	}
@@ -139,11 +140,12 @@ namespace ec
 	inline bool str_ineq(const char* s1, const char* s2, size_t n)
 	{
 		size_t i = 0;
-		while (i++ < n && *s1 && *s2) {
+		while (i < n && *s1 && *s2) {
 			if (*s1 != *s2 && tolower(*s1) != tolower(*s2))
 				return false;
 			s1++;
 			s2++;
+			i++;
 		}
 		return i == n;
 	}
