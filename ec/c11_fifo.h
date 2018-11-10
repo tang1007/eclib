554a8339c1499bcff12c37bc86386a88116e2b3d
kipway <kipway@outlook.com>
2018-11-10 15:46:14 +0800

    std::mutex -> ec:spinlock

--- ec/c11_fifo.h
+++ ec/c11_fifo.h
@@ -31,7 +31,7 @@ namespace ec
 	{
 	public:
 		typedef _Ty	value_type;
-		fifo(size_t usize, std::mutex *pmutex = nullptr) :_pmutex(pmutex) {
+		fifo(size_t usize, spinlock *pmutex = nullptr) :_pmutex(pmutex) {
 			_pbuf = new value_type[usize];
 			if (_pbuf)
 				_usize = usize;
@@ -48,22 +48,22 @@ namespace ec
 		};
 	private:
 		value_type * _pbuf;
-		std::mutex* _pmutex;
+		ec::spinlock* _pmutex;
 		size_t	_usize; //bufsize
 		size_t	_uhead;	//out
 		size_t	_utail;	//in,point empty
 	public:
 		bool empty() const noexcept {
-			unique_lock lck(_pmutex);
+			unique_spinlock lck(_pmutex);
 			return _uhead == _utail;
 		};
 		bool full() const noexcept {
-			unique_lock lck(_pmutex);
+			unique_spinlock lck(_pmutex);
 			return  (_utail + 1) % _usize == _uhead;
 		};
 		int add(value_type &item, bool *pbfull = nullptr) noexcept // -1:error;  0: full ;  1:success
 		{
-			unique_lock lck(_pmutex);
+			unique_spinlock lck(_pmutex);
 			if (!_pbuf)
 				return -1; // error
 			if ((_utail + 1) % _usize == _uhead) {
@@ -78,7 +78,7 @@ namespace ec
 			return 1; //success
 		};
 		void add_overflow(value_type &item) {
-			unique_lock lck(_pmutex);
+			unique_spinlock lck(_pmutex);
 			if (!_pbuf)
 				return; // error
 			if ((_utail + 1) % _usize == _uhead) { // full
@@ -90,7 +90,7 @@ namespace ec
 		}
 		bool get(value_type& item) noexcept
 		{
-			unique_lock lck(_pmutex);
+			unique_spinlock lck(_pmutex);
 			if (!_pbuf || _uhead == _utail)
 				return false;
 			item = _pbuf[_uhead];
@@ -100,7 +100,7 @@ namespace ec
 		}
 		void clear() noexcept
 		{
-			unique_lock lck(_pmutex);
+			unique_spinlock lck(_pmutex);
 			if (!_pbuf || _uhead == _utail)
 				return;
 			while (_uhead != _utail) {
@@ -112,7 +112,7 @@ namespace ec
 		}
 		size_t count() noexcept
 		{
-			unique_lock lck(_pmutex);
+			unique_spinlock lck(_pmutex);
 			size_t uh = _uhead;
 			size_t n = 0;
 			while (uh != _utail)
@@ -124,7 +124,7 @@ namespace ec
 		}
 		void for_each(std::function<void(value_type& val)> fun) noexcept
 		{
-			unique_lock lck(_pmutex);
+			unique_spinlock lck(_pmutex);
 			if (!_pbuf)
 				return;
 			size_t h = _uhead, t = _utail;
