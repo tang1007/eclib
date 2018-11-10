922e985c4be0114b20b4ca0b9ecef7109ac504f5
kipway <kipway@outlook.com>
2018-11-10 15:57:39 +0800

    add class spinlock

--- ec/c11_mutex.h
+++ ec/c11_mutex.h
@@ -2,7 +2,7 @@
 \file c11_mutex.h
 \author	jiangyong
 \email  kipway@outlook.com
-\update 2018.5.27
+\update 2018.10.6
 
 eclib class mutex with c++11.
 
@@ -24,18 +24,74 @@ limitations under the License.
 #pragma once
 
 #include <mutex>
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <pthread.h>
+#endif
 namespace ec {
 	class unique_lock {
 	private:
 		std::mutex *_pmutex;
 	public:
-		unique_lock(std::mutex *pmutex)  : _pmutex(pmutex){
+		unique_lock(std::mutex *pmutex) : _pmutex(pmutex) {
 			if (_pmutex)
 				_pmutex->lock();
 		}
 		~unique_lock() {
 			if (_pmutex)
 				_pmutex->unlock();
+		}
+	};
+
+#ifdef _WIN32
+	class spinlock
+	{
+	public:
+		spinlock() {
+			InitializeCriticalSectionAndSpinCount(&_v, UINT_MAX);
+		}
+		~spinlock() {
+			DeleteCriticalSection(&_v);
+		}
+	public:
+		void lock() { EnterCriticalSection(&_v); }
+		void unlock() { LeaveCriticalSection(&_v); }
+	private:
+		CRITICAL_SECTION _v;
+	};
+#else
+	class spinlock {
+	public:
+		spinlock() {
+			pthread_spin_init(&_v, PTHREAD_PROCESS_PRIVATE);
+		}
+		~spinlock() {
+			pthread_spin_destroy(&_v);
+		}
+	public:
+		void lock() {
+			pthread_spin_lock(&_v);
+		}
+		void unlock() {
+			pthread_spin_unlock(&_v);
+		}
+	private:
+		pthread_spinlock_t  _v;
+	};
+#endif
+
+	class unique_spinlock {
+	private:
+		spinlock *_plck;
+	public:
+		unique_spinlock(spinlock *plck) : _plck(plck) {
+			if (_plck)
+				_plck->lock();
+		}
+		~unique_spinlock() {
+			if (_plck)
+				_plck->unlock();
 		}
 	};
 }
